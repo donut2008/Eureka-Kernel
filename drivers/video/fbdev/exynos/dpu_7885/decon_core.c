94687b5a4c5cff24f58117a1a5d1d66de8d782a1
donut2008 <anjishnu.adhikary@outlook.com>
2025-06-12 20:06:09 +0530

    Revert "Add support for AOD"
    This reverts commit f9f2cd0059735a06f6ccbba6a9c0dd95648bb71c.

--- drivers/video/fbdev/exynos/dpu_7885/decon_core.c
+++ drivers/video/fbdev/exynos/dpu_7885/decon_core.c
@@ -441,7 +441,7 @@ int decon_tui_protection(bool tui_en)
 }
 
 /* ---------- FB_BLANK INTERFACE ----------- */
-int decon_enable(struct decon_device *decon)
+static int decon_enable(struct decon_device *decon)
 {
 	struct decon_mode_info psr;
 	struct decon_param p;
@@ -577,7 +577,7 @@ err:
 	return ret;
 }
 
-int decon_disable(struct decon_device *decon)
+static int decon_disable(struct decon_device *decon)
 {
 	struct decon_mode_info psr;
 	int ret = 0;
@@ -684,10 +684,8 @@ int decon_disable(struct decon_device *decon)
 	decon_runtime_suspend(decon->dev);
 #endif
 	decon->state = DECON_STATE_OFF;
-	decon_info("%s: decon->state is DECON_STATE_OFF\n", __func__);
 #if defined(CONFIG_EXYNOS_DOZE)
 	decon->doze_state = DOZE_STATE_SUSPEND;
-	decon_info("%s: decon->doze_state is DOZE_STATE_SUSPEND\n", __func__);
 #endif
 
 #ifdef CONFIG_EXYNOS_PD
@@ -2160,7 +2158,7 @@ static int decon_ioctl(struct fb_info *info, unsigned int cmd,
 			ret = -EFAULT;
 			break;
 		}
-		ret = doze != DOZE_STATE_DOZE_SUSPEND ? decon_set_doze_mode(decon, doze) : 0;
+		ret = decon_set_doze_mode(decon, doze);
 		if (ret)
 			decon_err("DECON:ERR:%s:failed to set doze mode\n", __func__);
 #endif
