921e7f881ca837db2727e954286f747ed417ab76
donut6150 <76439683+donut6150@users.noreply.github.com>
2026-07-13 18:17:14 +0530

    AK3: Update flashing script

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -48,19 +48,17 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 #Method 2:
 split_boot;
 ui_print "- Installing donut kernel";
+ui_print " ";
+
+ui_print "- Flashing boot image";
+ui_print " ";
 flash_boot;
 
-## Copy additional files to internal storage
-cp /tmp/anykernel/tools/htweaks.zip /data/media/0/Hiyorix_Tweaks.zip;
-chmod 755 /data/media/0/Hiyorix_Tweaks.zip;
+ui_print "- Flashing DTB";
+ui_print " ";
+flash_dtb;
 
 ui_print "- Installation finished successfully!";
-ui_print " ";
-
-ui_print "- Thank you for using donut kernel!";
-ui_print " ";
-
-ui_print "- Flash Hiyorix_Tweaks.zip found on your internal storage for maximum performance.";
 ui_print " ";
 
 ## end install
